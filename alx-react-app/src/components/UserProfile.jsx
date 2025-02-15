fcc3f99b692f6028d5047a268697ace4fdebed8f
Eninnaya Chioma <enyinnayachioma738@gmail.com>
2025-02-16 00:36:36 +0100

    Added the userprofile

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,21 +1,12 @@
-
-import PropTypes from 'prop-types';
-
-function UserProfile({name,age,bio}) {
+const UserProfile = (props)=>{
   return (
    
     <div>
-        <h2>Name:{name}</h2>
-       <p>Age: {age}</p>
-       <p>Bio: {bio}</p>
+        <h2>Name:{props.name}</h2>
+       <p>Age: {props.age}</p>
+       <p>Bio: {props.bio}</p>
     </div>
   )
 }
 
-
-UserProfile.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.string.isRequired,
-  bio: PropTypes.string.isRequired, // Ensure name is a required string
-};
 export default UserProfile
