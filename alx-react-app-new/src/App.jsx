ddfc7d65dfd61c63614782b1509345d0c868ece5
Eninnaya Chioma <enyinnayachioma738@gmail.com>
2025-02-23 20:35:46 +0100

    Created counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,7 +1,7 @@
-
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent'
+import Counter from './components/Counter';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import './App.css'
@@ -13,6 +13,7 @@ function App() {
     <>
     <Header />
     <MainContent />
+    <Counter/>
     <Footer />
     <WelcomeMessage />
     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
