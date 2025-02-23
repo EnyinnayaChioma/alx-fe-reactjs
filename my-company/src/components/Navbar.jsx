79ab870ec0ae0a9fb64800b704ecea14e04acda1
Eninnaya Chioma <enyinnayachioma738@gmail.com>
2025-02-23 23:46:13 +0100

    did necessary task

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#333', padding: '10px', color: '#fff', justifyContent: 'space-between' }}>
+    <nav style={{ backgroundColor: '#333', padding: '10px', color: '#fff', display: 'flex', justifyContent: 'space-between' }}>
       <Link to="/" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Home</Link>
       <Link to="/about" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>About</Link>
       <Link to="/services" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Services</Link>
