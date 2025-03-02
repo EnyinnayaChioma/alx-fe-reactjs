ced6acd9aa35ab335bf2a6d6a41121b55ac9ab00
Eninnaya Chioma <enyinnayachioma738@gmail.com>
2025-03-02 23:30:16 +0100

    Created the recipe sharing appt

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,25 +1,29 @@
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent'
-import Counter from './components/Counter';
+import Counter from './components/Counter'
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+
+
+// import Counter from './components/Counter';
+
 import './App.css'
 
 function App() {
-
-
   return (
     <>
+
     <Header />
     <MainContent />
     <Counter/>
     <Footer />
     <WelcomeMessage />
     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      
+
     </>
   )
 }
+
 
 export default App
