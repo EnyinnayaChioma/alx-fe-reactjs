c2beb50ea357adc696868c41481f9617b6435a9f
Eninnaya Chioma <enyinnayachioma738@gmail.com>
2025-03-03 02:06:21 +0100

    made corrections

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,26 @@
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import './App.css';
+// Install first: npm install react-router-dom
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app-container">
-      <h1> Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home 🏠</Link>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<>
+          <AddRecipeForm />
+          <RecipeList />
+        </>} />
+        
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/edit/:id" element={<EditRecipeForm />} />
+      </Routes>
+    </Router>
   );
 }
 
