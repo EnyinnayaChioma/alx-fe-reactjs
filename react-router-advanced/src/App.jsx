1633d787828b1b8995879797fc4f66d087fa0172
Eninnaya Chioma <enyinnayachioma738@gmail.com>
2025-03-09 23:02:55 +0100

    made corrections

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,12 +1,9 @@
+// App.jsx
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Login from './components/Login';
 import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
-import Posts from './components/Posts';
-import Post from './components/Post';
 import ProtectedRoute from './components/ProtectedRoute';
 
 const router = createBrowserRouter([
@@ -20,15 +17,11 @@ const router = createBrowserRouter([
         path: 'profile',
         element: <ProtectedRoute><Profile /></ProtectedRoute>,
         children: [
-          { index: true, element: <ProfileDetails /> },
-          { path: 'details', element: <ProfileDetails /> },
-          { path: 'settings', element: <ProfileSettings /> },
-        ],
-      },
-      { path: 'posts', element: <Posts /> },
-      { path: 'posts/:postId', element: <Post /> },
-    ],
-  },
+          // Add nested routes here
+        ]
+      }
+    ]
+  }
 ]);
 
 function App() {
